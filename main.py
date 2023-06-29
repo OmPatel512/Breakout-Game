5507dcd2d0296ba57fbcece049813faa24f14368
om patel <omsuperson@gmail.com>
2023-06-29 20:11:25 +0530

    Paddle has been added.

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
-from turtle import Screen
+from turtle import Screen, Turtle
+from paddle import Paddle
 
 screen = Screen()
 screen.bgcolor("black")
 screen.setup(height=700, width=500)
 screen.title("Breakout Game")
+screen.tracer(0)
 
+paddle = Paddle()
 
+screen.listen()
+screen.onkey(paddle.go_left, "Left")
+screen.onkey(paddle.go_right, "Right")
 
+game_running = True
+while game_running:
+    screen.update()
 
 screen.exitonclick()
