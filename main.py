a6d648e46908b486a54c148874e9b305fd29b668
om patel <omsuperson@gmail.com>
2023-06-30 22:00:15 +0530

    Ball has been added, and it's mechanism has been set. There is a problem in brick.py will be fixed in next iteration.

--- main.py
+++ main.py
@@ -1,20 +1,90 @@
+import time
 from turtle import Screen, Turtle
 from paddle import Paddle
+from brick import Brick
+from ball import Ball
 
 screen = Screen()
 screen.bgcolor("black")
-screen.setup(height=700, width=500)
+screen.setup(height=700, width=540)
 screen.title("Breakout Game")
 screen.tracer(0)
 
 paddle = Paddle()
+ball = Ball()
+
 
 screen.listen()
 screen.onkey(paddle.go_left, "Left")
 screen.onkey(paddle.go_right, "Right")
 
+
+def check_collision_walls():
+    global ball
+
+    if ball.xcor() < -260 or ball.xcor() > 260:
+        ball.bounce(x_bounce=True, y_bounce=False)
+        return
+
+    if ball.ycor() > 340:
+        ball.bounce(x_bounce=False, y_bounce=True)
+        return
+
+    if ball.ycor() < -330:
+        ball.reset()
+        return
+
+
+def check_collision_paddle():
+
+    global ball, paddle
+
+    ball_x = ball.xcor()
+    paddle_x = paddle.xcor()
+
+    if ball.distance(paddle) < 50 and ball.ycor() < -290:
+
+        #if Paddle is on right side of screen.
+        if paddle_x > 0:
+            if ball_x > paddle_x:
+                ball.bounce(x_bounce=True, y_bounce=True)
+                return
+            else:
+                ball.bounce(x_bounce=False, y_bounce=True)
+                return
+
+        #if Paddle is on left side of screen.
+        elif paddle_x < 0:
+            if ball_x > paddle_x:
+                ball.bounce(x_bounce=True, y_bounce=True)
+                return
+            else:
+                ball.bounce(x_bounce=False, y_bounce=True)
+                return
+
+        #Else paddle is in the middle.
+        else:
+            if ball_x > paddle_x:
+                ball.bounce(x_bounce=True, y_bounce=True)
+                return
+            elif ball_x > paddle_x:
+                ball.bounce(x_bounce=True, y_bounce=True)
+                return
+            else:
+                ball.bounce(x_bounce=False, y_bounce=True)
+                return
+
+def check_collision_bricks():
+    pass
+
+
 game_running = True
 while game_running:
     screen.update()
+    time.sleep(0.05)
+    ball.move()
+
+    check_collision_walls()
+    check_collision_paddle()
 
 screen.exitonclick()
--- paddle.py
+++ paddle.py
@@ -1,7 +1,6 @@
 from turtle import Turtle
 
 class Paddle(Turtle):
-
     def __init__(self):
         super().__init__()
         self.shape("square")
@@ -11,9 +10,9 @@ class Paddle(Turtle):
         self.goto(x=0, y=-310)
 
     def go_left(self):
-        new_x = self.xcor() - 25
+        new_x = self.xcor() - 35
         self.goto(new_x, self.ycor())
 
     def go_right(self):
-        new_x = self.xcor() + 25
+        new_x = self.xcor() + 35
         self.goto(new_x, self.ycor())
